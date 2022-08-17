96abb543046d8c35df1f07cc92d31b4cfc708f3e
manishareddy842 <109997654+manishareddy842@users.noreply.github.com>
2022-08-17 17:25:50 -0500

    added eventLinster to the code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
-function displayForecastWeatherTemp(){
-let forecastElement=document.querySelector("#forecast") 
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let latest = new Date();
+        let date = latest.getDate();
+        let time = latest.getTime();
+        let hour = latest.getHours();
+        let minutes = latest.getMinutes();
+        let day = days[latest.getDay()];
+        let then = document.querySelector("#day");
+        then.innerHTML = `${day} ${hour} ${minutes}`
+
+function displayForecast(){
+        let forecastElement=document.querySelector("#forecast") 
 let forecast=`<div class="row">`
 let daysin=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 daysin.forEach(function(day) {
@@ -19,17 +29,21 @@ forecast=forecast +`
 forecast=forecast+`</div>`;
     forecastElement.innerHTML=forecast;
     console.log(forecast)
-
 }
-
+function getForecast(coordinates){
+console.log(coordinates)
+let apiKey= "eb5b124d80c197ea4ca7358f030ee172";
+let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast)
+}
 function showTemperature(response) {
             console.log(response);
-           let anything= document.querySelector(".place")
+           let cityElement= document.querySelector("#place")
            let temperatureElement=document.querySelector("#temperature")
            let humidityElement=document.querySelector("#humidity")
            let windElement=document.querySelector("#wind")
            let descriptionElement=  document.querySelector("#of")
-           anything.innerHTML = response.data.name;
+           cityElement.innerHTML = response.data.name;
            celsius = response.data.main.temp ;
            temperatureElement.innerHTML=celsius;
            humidityElement.innerHTML = response.data.main.humidity;
@@ -41,31 +55,33 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
         }
-          
-        let apiKey = "eb5b124d80c197ea4ca7358f030ee172";
-            let input = document.querySelector("#city-Input").value;
-            let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&q=${input}&appid=${apiKey}&units=metric`;
-            axios.get(apiUrl).then(showTemperature);
-            displayForecastWeatherTemp();
-          
-          let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        let latest = new Date();
-        let date = latest.getDate();
-        let time = latest.getTime();
-        let hour = latest.getHours();
-        let minutes = latest.getMinutes();
-        let day = days[latest.getDay()];
-        let then = document.querySelector("#day");
-        then.innerHTML = `${day} ${hour} ${minutes}`
+
+function search(city) {
+
+  let apiKey = "eb5b124d80c197ea4ca7358f030ee172";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+  getForecast(coords);
+  displayForecast();
+}
+
+function handlesubmit(event){
+    event.preventDefault()
+ let cityInputElement = document.querySelector("#city-Input"); 
+ search(cityInputElement.value)  
+}
+let form=document.querySelector("#form")
+form.addEventListener("submit",handlesubmit)
+
 function changeToFarenhite(event){
     event.preventDefault()
 let temperatureElement=document.querySelector("#temperature") 
-let farenhiteTemp=(temperatureElement.innerHTML * 9/5) + 32;
+let farenhiteTemp=(celsius* 9/5) + 32;
 temperatureElement.innerHTML=Math.round(farenhiteTemp);
 }
 let tempChange=document.querySelector("#farenhite")
 tempChange.addEventListener("click",changeToFarenhite)
- let celsiusLink=null;
+ let celsius=null;
 
  function changeToCelsius(event){
 event.preventDefault()
